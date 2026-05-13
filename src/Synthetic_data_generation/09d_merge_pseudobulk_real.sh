032f752fc290223c15a3898ed777aa6d80c8afc7
Valentin Muller <vmuller@login8.hpc.binf.unibe.ch>
2026-05-13 10:31:13 +0200

    Add docstring

--- src/Synthetic_data_generation/09d_merge_pseudobulk_real.sh
+++ src/Synthetic_data_generation/09d_merge_pseudobulk_real.sh
@@ -3,24 +3,22 @@
 #SBATCH --time=01:00:00
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=20G
-#SBATCH --job-name=merge_pseudobulk
-#SBATCH --output=/data/users/vmuller/0_master_thesis/log/merge_pseudobulk_%J.out
-#SBATCH --error=/data/users/vmuller/0_master_thesis/log/merge_pseudobulk_%J.err
+#SBATCH --job-name=merge_pseudobulk_real
+#SBATCH --output=/data/users/vmuller/0_master_thesis/log/merge_pseudobulk_real_%J.out
+#SBATCH --error=/data/users/vmuller/0_master_thesis/log/merge_pseudobulk_real_%J.err
 
 
 set -euo pipefail
-
 echo $(date)
 
 # Load the configuration variables and activate the conda environment
 source "$SLURM_SUBMIT_DIR/00_config.sh"
 activate_conda
 
-# Define the input directories for the pseudobulk RNA and ATAC data and the output directory for the merged pseudobulk data
-INPUT_RNA_DIR="/data/users/vmuller/0_master_thesis/data/SEAD_Dataset/patient_subsets/real_pseudobulk/RNA"
-INPUT_ATAC_DIR="/data/users/vmuller/0_master_thesis/data/SEAD_Dataset/patient_subsets/real_pseudobulk/ATAC"
-OUTPUT_DIR="/data/users/vmuller/0_master_thesis/data/SEAD_Dataset/patient_subsets/real_pseudobulk_merged"
-
+# Define the input directories for the real pseudobulk RNA and ATAC data and the output directory for the merged real pseudobulk data
+INPUT_RNA_DIR="$PATH_DONOR_SUBSETS/real_pseudobulk/RNA"
+INPUT_ATAC_DIR="$PATH_DONOR_SUBSETS/real_pseudobulk/ATAC"
+OUTPUT_DIR="$PATH_DONOR_SUBSETS/real_pseudobulk_merged"
 mkdir -p "$OUTPUT_DIR"
 
 python "$PATH_SCRIPTS/09d_merge_pseudobulk_real.py" \
@@ -29,3 +27,4 @@ python "$PATH_SCRIPTS/09d_merge_pseudobulk_real.py" \
     "$OUTPUT_DIR"
 
 echo "Finished"
+echo $(date)
